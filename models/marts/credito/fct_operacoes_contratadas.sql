3450cb486009a10b685dc23dbbb54760a6f73864
Juan perri <111884460+perrijuan@users.noreply.github.com>
2026-07-19 18:45:21 -0300

    Update fct_operacoes_contratadas.sql

--- models/marts/credito/fct_operacoes_contratadas.sql
+++ models/marts/credito/fct_operacoes_contratadas.sql
@@ -1,11 +1,9 @@
 {{ config(
-    materialized = 'incremental',
-    incremental_strategy = 'insert_overwrite',
+    materialized = 'table',
     partition_by = {'field': 'sk_data', 'data_type': 'date', 'granularity': 'month'},
-    cluster_by = ['sk_municipio', 'sk_cliente'],
-    on_schema_change = 'append_new_columns'
+    cluster_by = ['sk_municipio', 'sk_cliente']
 ) }}
-
+    
 select
     -- chaves estrangeiras
     data_contratacao                          as sk_data,
@@ -23,9 +21,3 @@ select
     safe_divide(valor_desembolsado, valor_contratado) as taxa_desembolso
 
 from {{ ref('stg_bndes__operacoes') }}
-
-{% if is_incremental() %}
-where data_contratacao >= (
-    select date_sub(max(sk_data), interval 3 month) from {{ this }}
-)
-{% endif %}
